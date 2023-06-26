270cd3a6cdb286077a205219518e8fc9b28db061
boonjun tan <boonjun.tan@zoom.us>
2023-06-26 17:28:08 +0800

    feat: Update latest framework

--- Package.swift
+++ Package.swift
@@ -4,33 +4,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "ZoomVideoSDKiOSUIKit",
+    name: "ZoomVideoSDKUIToolkit-iOS",
     platforms: [
         .iOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "ZoomVideoSDKiOSUIKit",
-            targets: ["ZoomVideoSDKiOSUIKit"]),
+            name: "ZoomVideoSDKUIToolkit-iOS",
+            targets: ["ZoomVideoSDK", "ZoomVideoSDKUIToolkit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/zoom/zoom-video-sdk-iOS.git",
-                 branch: "swift-package-manager"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "ZoomVideoSDKiOSUIKit",
-            dependencies: [
-                .product(name: "ZoomVideoSDK-iOS", package: "zoom-video-sdk-iOS")
-            ],
-            path: "Sources"),
-        .testTarget(
-            name: "ZoomVideoSDKiOSUIKitTests",
-            dependencies: ["ZoomVideoSDKiOSUIKit"]),
+        .binaryTarget(
+            name: "ZoomVideoSDK",
+            path: "ZoomVideoSDK.xcframework"),
+        .binaryTarget(
+            name: "ZoomVideoSDKUIToolkit",
+            path: "ZoomVideoSDKUIToolkit.xcframework"),
     ]
 )
